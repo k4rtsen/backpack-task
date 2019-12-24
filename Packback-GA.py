6d781c2d3cf0afcf45f789747e1297e67657e71e
Kartsen <36922041+k4rtsen@users.noreply.github.com>
2019-12-24 12:57:13 +0300

    Завершенный проект. Реализованн класс bot.

--- Packback-GA.py
+++ Packback-GA.py
@@ -1,77 +1,118 @@
 import random
+NACH_ZN = 10
+PROC_ADD = 10
+PROC_DEL = 10
 
+class bot:      
+    def __init__(self):
+        self.chrom = []
+        self.wei = 0
+        self.price = 0
+    
+    def set_chrom(self):
+        for _ in range(count):
+            self.chrom.append(random.randint(0,1))
+    
+    #Подсчет весов и ценностей
+    def sum_bot(self):
+        for i in range(len(self.chrom)):
+            if self.chrom[i] == 1:
+                self.wei += things[i][0]
+                self.price += things[i][1]
+
+    #Скрещивание хромосом 50\50
+    def cross_chrom(self, arr1, arr2):
+        p = count // 2
+        for i in range(p):
+            self.chrom.append(arr1[i])
+        for i in range(p, count):
+            self.chrom.append(arr2[i])
+
+    #Мутация в 25%
+    def mutation(self):
+        if (random.randint(0, 99) < 25):
+            r = random.randint(0, len(self.chrom) - 1)
+            if (self.chrom[r] == 0): self.chrom[r] = 1
+            else: self.chrom[r] = 0
+
+#Создание начального количества ботов
+def create_nz():
+    for i in range(NACH_ZN):
+        bots.append(bot())
+        #Добавление 0 и 1 в хромосомы
+        bots[i].set_chrom()
+        #Вес и ценность каждой выборки
+        bots[i].sum_bot()
+    
+#Сортировка и вывод наилучшего решения
+def search(lst):
+    lst.sort(reverse=True, key=lambda bot: bot.price)
+    for i in range(len(lst)):
+        if lst[i].wei < packback:
+            return i, lst[i].wei, lst[i].price, lst[i].chrom
+        else: flag = True
+    if flag: return 'Любая выборка превыщает размер рюкзака.'
+
+#Добавление новых ботов (PROC от общего количества)
+def add_bots(arr):
+    nz = len(arr)
+    end = len(arr) / 100 * PROC_ADD
+    x = y = -1
+    #Добавление 10%
+    for i in range(nz, nz + int(end)):
+        while (x == y):
+            x = random.randint(0, nz - 1)
+            y = random.randint(0, nz - 1)
+        #Добавление бота
+        arr.append(bot())
+        #Скрещиваем хромосом родителей (x и y)
+        arr[i].cross_chrom(arr[x].chrom, arr[y].chrom)
+        #Подвергаем мутации
+        arr[i].mutation()
+        #Сразу узнаем вес и ценность для добавленного бота
+        arr[i].sum_bot()
+
+#Удаление 5% наихудших ботов от общего количества
+def delete_bots(arr):
+    dead = len(arr) / 100 * PROC_DEL
+    dead = int(dead)
+    while (dead != 0):
+        for i in range(len(arr)):
+            if arr[i].wei > packback:
+                arr.pop(i)
+                dead -= 1
+                flag = False
+                break
+            else: flag = True
+        if flag: 
+            arr.pop()
+            dead -= 1
+
+
+#MAIN
+#НАЧАЛО ПРОГРАММЫ
 packback = int(input('Введите размер рюкзака: '))
 count = int(input('Введите количество вещей: '))
 things = []
-bot = []
-
-#Поиск наилучшего решения
-def search_best():
-    index = -1
-    max_wei = max_price = 0
-    for i in range(len(bot)):
-        price = wei = 0
-        for j in range(count):
-            if (bot[i][j] == 1):
-                wei += things[j][0]
-                price += things[j][1]
-        if (packback >= wei):
-            if (price > max_price):
-                max_price = price
-                max_wei = wei
-                index = i + 1
-    if (index != -1):
-        return 'Вес: ' + str(max_wei) + ' Цена: ' + str(max_price) + ' Номер бота: ' + str(index)
-    else: return 'Любая выборка превышает размер рюкзака.'
-#------------------------------------------------------------
-
-#Создание новых ботов(скрещивание). 10% от общего количество
-def create_new_bots(size):
-    a = b = -1
-    while a == b:
-        a = random.randint(0, size - 1)
-        b = random.randint(0, size - 1)
-    end = size // 10
-    for i in range(size, size + end):
-        bot.append([])
-        for j in range(count):
-            if (random.randint(0,1) == 0): bot[i].append(bot[a][j])
-            else: bot[i].append(bot[b][j])
-        mutation(i)
-#------------------------------------------------------------
-
-#Мутация в 0,25% случаях
-def mutation(index):
-    if (random.randint(1,100) < 25):
-        if (bot[index - 1][random.randint(0, count - 1)] == 1): bot[index - 1][random.randint(0, count - 1)] = 0
-        else: bot[index - 1][random.randint(0, count - 1)] = 1
-#------------------------------------------------------------
-
+bots = []
 #Инициализация таблицы вещей, веса которых от 1 до 9, а ценности от 10 до 100
 for i in range(count):
     things.append([])
-    for j in range(2):
-        if (j == 0):
-            r = random.randint(1,9)
-            things[i].append(r)
-        else:
-            r = random.randint(10,100)
-            things[i].append(r)
+    things[i].append(random.randint(1,9))
+    things[i].append(random.randint(10,100))
 
-#Инициализация хромосом ботов 0 и 1
-for i in range(10):
-    bot.append([])
-    for j in range(count):
-        bot[i].append(random.randint(0,1))
-
+#Создаем NACH_ZN ботов
+create_nz()
+print(search(bots))
+for _ in range(1000):
+    #Сортировка и вывод наилучшего решения
+    search(bots)
+    #Добавляем новых ботов
+    add_bots(bots)
+    search(bots)
+    #Удалинеие худших ботов
+    delete_bots(bots)
+    
+print(search(bots))
 print(things)
-bot.sort()
-
-for i in range(100):
-    print(i, ': ', search_best())
-    create_new_bots(len(bot))
-
-for i in range(len(bot)):
-    print(i, 'bot: ', bot[i])
-
 input('Press ENTER to exit...')
