8704b57538b6bfbc1db19d2a0962292affcfeb36
Karen Ayvazyan <karen.ayvazyan@neuro-city.ru>
2022-09-09 09:32:16 +0300

    some changes

--- Packback-GA.py
+++ Packback-GA.py
@@ -1,14 +1,20 @@
+from tabulate import tabulate #модуль для таблицы
 import random
-NACH_ZN = 10
-PROC_ADD = 10
-PROC_DEL = 10
+import time
 
-class bot:      
+NACH_ZN = 1024 #Начальное значение
+PROC_ADD = 20 #Процент добавления новых особей
+PROC_DEL = 20 #Процент удаления плохих особей
+PM = 25 #Процент мутации для каждого элемента хромосомы
+
+class bot:
+    #конструктор      
     def __init__(self):
         self.chrom = []
         self.wei = 0
         self.price = 0
-    
+
+    #Задание генов в хромосомах 0 и 1
     def set_chrom(self):
         for _ in range(count):
             self.chrom.append(random.randint(0,1))
@@ -28,12 +34,12 @@ class bot:
         for i in range(p, count):
             self.chrom.append(arr2[i])
 
-    #Мутация в 25%
+    #Мутация в 25% для каждой хромосомы
     def mutation(self):
-        if (random.randint(0, 99) < 25):
-            r = random.randint(0, len(self.chrom) - 1)
-            if (self.chrom[r] == 0): self.chrom[r] = 1
-            else: self.chrom[r] = 0
+        for r in range(len(self.chrom)):
+            if (random.randint(0, 99) < PM):
+                if (self.chrom[r] == 0): self.chrom[r] = 1
+                else: self.chrom[r] = 0
 
 #Создание начального количества ботов
 def create_nz():
@@ -49,7 +55,7 @@ def search(lst):
     lst.sort(reverse=True, key=lambda bot: bot.price)
     for i in range(len(lst)):
         if lst[i].wei < packback:
-            return i, lst[i].wei, lst[i].price, lst[i].chrom
+            return lst[i].wei, lst[i].price, lst[i].chrom
         else: flag = True
     if flag: return 'Любая выборка превыщает размер рюкзака.'
 
@@ -71,8 +77,9 @@ def add_bots(arr):
         arr[i].mutation()
         #Сразу узнаем вес и ценность для добавленного бота
         arr[i].sum_bot()
+    
 
-#Удаление 5% наихудших ботов от общего количества
+#Удаление наихудших ботов от общего количества
 def delete_bots(arr):
     dead = len(arr) / 100 * PROC_DEL
     dead = int(dead)
@@ -88,31 +95,54 @@ def delete_bots(arr):
             arr.pop()
             dead -= 1
 
-
 #MAIN
-#НАЧАЛО ПРОГРАММЫ
 packback = int(input('Введите размер рюкзака: '))
 count = int(input('Введите количество вещей: '))
-things = []
-bots = []
+
+things = [] #Список вещей
+bots = [] #Популяция
+
 #Инициализация таблицы вещей, веса которых от 1 до 9, а ценности от 10 до 100
 for i in range(count):
     things.append([])
     things[i].append(random.randint(1,9))
     things[i].append(random.randint(10,100))
 
+print("\nСписок предметов:")
+#Организация списка предметов в виде таблицы
+print(tabulate(things, headers=['Вес', 'Цена'], tablefmt="grid"))
+#print(things)
+
+'''НАЧАЛО ГЕНЕТИЧЕСКОГО АЛГОРИТМА'''
 #Создаем NACH_ZN ботов
 create_nz()
-print(search(bots))
-for _ in range(1000):
-    #Сортировка и вывод наилучшего решения
+best = search(bots)
+if isinstance(best, str): print("\nНачальная популяция: ", best)
+else: print("\nЛучшее решение для начальной популяции:", "\n- вес сбоки: ", best[0], "\n- ценность сбоки: ", best[1], "\n- предметы сбоки : ", best[2])
+iterat = 0
+cikl = 30 #размер цикла
+#Засекаем время выполнения программы
+start_time = time.time()
+while (cikl > 0):
+    iterat += 1
     search(bots)
     #Добавляем новых ботов
     add_bots(bots)
     search(bots)
     #Удалинеие худших ботов
     delete_bots(bots)
-    
-print(search(bots))
-print(things)
-input('Press ENTER to exit...')
+    cikl -= 1
+    if isinstance(search(bots), str): 
+        cikl += 1
+        if iterat == 500:
+            break
+
+#Остановка времени
+stop = time.time() - start_time
+best = search(bots)
+if isinstance(best, str): print("\nКонечная популяция популяция: ", best)
+else: print("\nЛучшее решение для конечной популяции:", "\n- вес сбоки: ", best[0], "\n- ценность сбоки: ", best[1], "\n- предметы сбоки : ", best[2])
+print("\nПрожитых поколений: ", iterat)
+print("Время выполнения генетического алгоритма: ")
+print("--- %s seconds ---" % (stop))
+input("\nPress ENTER to exit...")
